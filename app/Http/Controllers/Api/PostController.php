7cea3654b938a984816eb518643c6f3ee2b14edd
coszach@hotmail.it <coszach@hotmail.it>
2022-05-09 14:50:52 +0200

    add index PostController + route in api.php

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,7 +15,12 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::with(['category', 'tags'])->limit(12)->get();
+        
+        return response()->json([
+            'post' => $posts,
+            'success' => true
+        ]);
     }
 
     /**
